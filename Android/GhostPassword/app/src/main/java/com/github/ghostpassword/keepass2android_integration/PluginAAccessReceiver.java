d6a9ff0818d6dc2a1ab489b14381efa10ac4cb51
Jeffrey DeYoung <Jeffrey.J.DeYoung@gmail.com>
2015-11-23 20:40:19 -0700

    nightly stopping point

--- Android/GhostPassword/app/src/main/java/com/github/ghostpassword/keepass2android_integration/PluginAAccessReceiver.java
+++ Android/GhostPassword/app/src/main/java/com/github/ghostpassword/keepass2android_integration/PluginAAccessReceiver.java
@@ -1,20 +1,18 @@
 package com.github.ghostpassword.keepass2android_integration;
 
-import java.util.ArrayList;
-
 import keepass2android.pluginsdk.Strings;
 
+import java.util.ArrayList;
+
 public class PluginAAccessReceiver
-        extends keepass2android.pluginsdk.PluginAccessBroadcastReceiver
-{
+        extends keepass2android.pluginsdk.PluginAccessBroadcastReceiver {
 
     @Override
     public ArrayList<String> getScopes() {
         ArrayList<String> scopes = new ArrayList<String>();
-        //scopes.add(Strings.SCOPE_DATABASE_ACTIONS);
+        scopes.add(Strings.SCOPE_DATABASE_ACTIONS);
         scopes.add(Strings.SCOPE_CURRENT_ENTRY);
         return scopes;
 
     }
-
 }
